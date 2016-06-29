96141b715b8e3c2ea78863fab5ff0b2682fa16f4
Jonathon M. Abbott <jonathona@everydayhero.com.au>
2016-06-29 15:22:56 +1000

    WIP: post to gcloud instead of ES

--- index.js
+++ index.js
@@ -2,59 +2,33 @@ require("es6-promise").polyfill();
 
 var fetch = require("node-fetch");
 var doProcess = require("./process");
+var logger = require("./bulk-logger")
 
 var endpoint = process.env.ENDPOINT;
 
 exports.handler = function(input, context) {
   doProcess(input)
     .then(post)
+    .then(assumeWeWroteEverything)
     .then(summarize)
     .then(context.succeed, context.fail);
 };
 
-function post(bulk) {
-  var body = bulk.join("\n") + "\n";
-  var params = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Host": endpoint
-    },
-    body: body
-  };
-
-  return fetch(endpoint + "/_bulk", params);
+function assumeWeWroteEverything(bulk) {
+  return bulk.length
 }
 
-function summarize(res) {
-  console.log(res.ok);
-  console.log(res.status);
-  console.log(res.statusText);
+function post(bulk) {
+  return logger.write(bulk)
+    .then(function() { return bulk })
+}
 
-  return res.json().then(function(info) {
-    var failedItems, errors = {};
-
-    failedItems = info.items.filter(function(x) {
-      return x.status >= 300;
-    });
-
-    failedItems.forEach(function(x) {
-      errors[x.error.type] = x.error;
-    });
-
-    console.log({
-      attemptedItems: info.items.length,
-      successfulItems: info.items.length - failedItems.length,
-      failedItems: failedItems.length,
-      errors: values(errors)
-    });
-
-    if (res.ok) {
-      return Promise.resolve("Success");
-    } else {
-      return Promise.reject("Failed: " + res.statusText);
-    }
+function summarize(count) {
+  console.log({
+    successfulItems: count
   });
+
+  return "Success"
 }
 
 function values(object) {
