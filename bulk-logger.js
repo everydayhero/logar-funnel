1b7141c8632182f09a577409a97a979f153e09d2
Jonathon M. Abbott <jonathona@everydayhero.com.au>
2016-07-11 15:48:40 +1000

    Use GCLOUD_* instead of keyfile.json

--- bulk-logger.js
+++ bulk-logger.js
@@ -1,5 +1,8 @@
 var BulkLogger = require('gcloud-bulk-logger').default
 
-var logger = new BulkLogger('edh-logging-test', 'keyfile.json')
+var clientId = process.env.GCLOUD_EMAIL
+var privateKey = process.env.GCLOUD_PRIVATE_KEY
+
+var logger = new BulkLogger('edh-logging-test', clientId, privateKey)
 
 module.exports = logger
