98feb4320cfbd6839c0c5268c0d84efda2028008
Jonathon M. Abbott <jonathona@everydayhero.com.au>
2016-07-13 14:11:04 +1000

    Use GCLOUD_PROJECT_NAME for the project name

--- bulk-logger.js
+++ bulk-logger.js
@@ -2,7 +2,8 @@ var BulkLogger = require('gcloud-bulk-logger').default
 
 var clientId = process.env.GCLOUD_EMAIL
 var privateKey = process.env.GCLOUD_PRIVATE_KEY
+var projectName = process.env.GCLOUD_PROJECT_NAME
 
-var logger = new BulkLogger('edh-logging-test', clientId, privateKey)
+var logger = new BulkLogger(projectName, clientId, privateKey)
 
 module.exports = logger
